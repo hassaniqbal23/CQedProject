c6bdfc97695d33e5247d5483c30cdb1286af36f9
arslan <arslanshaheen1999@gmail.com>
2024-04-17 21:18:26 +0500

    arslan/Navbar-changes

--- components/common/navbar/navbar.stories.ts
+++ components/common/navbar/navbar.stories.ts
@@ -13,3 +13,17 @@ type Story = StoryObj<typeof navbar>;
 export const Default: Story = {
   args: {},
 };
+
+export const bottom: Story = {
+  args: {
+    variant: "Bottom navigation bar",
+    onClick: () => console.log("Bottom navigation bar"),
+  },
+};
+
+export const TopBar: Story = {
+  args: {
+    variant: "Top bar",
+    onClick: () => alert("open top bar"),
+  },
+};
