2cfab1fedf7ac831a122130a20e84386c57a7059
Ilyas Karim <ilyas.datoo@gmail.com>
2024-04-22 20:29:47 +0500

    Update Progressbar.tsx

--- components/common/Progressbar/Progressbar.tsx
+++ components/common/Progressbar/Progressbar.tsx
@@ -8,6 +8,18 @@ interface IProgressbarprops {
 }
 
 function Progressbar({ percentage, heading }: IProgressbarprops) {
+
+    const memorizedPercentage = React.useMemo(() => {
+        if (percentage > 100) {
+            return 100
+        }else {
+            if (percentage < 0) {
+                return 0
+            }
+            return percentage
+        }
+    }, [percentage])
+
     return (
         <div className='w-full rounded-full'>
             <div className='w-full bg-primary-50 rounded-full' >
@@ -15,7 +27,8 @@ function Progressbar({ percentage, heading }: IProgressbarprops) {
                     <div
                         className='absolute bg-secondary z-20 rounded-full transition-all duration-75 h-full'
                         style={{
-                            width: `${percentage}%`,
+                            width: `${ memorizedPercentage }%`,
+                            transition: 'width 0.75s',
                             padding: '0.5rem'
                         }}
                     >
@@ -41,7 +54,7 @@ function Progressbar({ percentage, heading }: IProgressbarprops) {
             </div>
             <div className='flex justify-between pt-5 pl-2 ' >
                 <h2 className={`font-semibold ` + (percentage === 100 ? 'text-success' : '')} >{heading}</h2>
-                <h2 >{percentage}% Completed</h2>
+                <h2 >{memorizedPercentage}% Completed</h2>
             </div>
         </div>
     );
