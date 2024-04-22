1cb4df33554c60ee1cde011ef4ba14c1a3f8fe12
Ilyas Karim <ilyas.datoo@gmail.com>
2024-04-22 20:37:49 +0500

    Update tabs.tsx

--- components/ui/tabs/tabs.tsx
+++ components/ui/tabs/tabs.tsx
@@ -35,10 +35,10 @@ const TabsTrigger = forwardRef<
 		<TabsPrimitive.Trigger
 			ref={ref}
 			className={cn(
-				`inline-flex items-center justify-center whitespace-nowrap py-2.5 px-7 text-base font-medium hover:ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-primary data-[state=active]:font-semibold `,
+				`border-b-2 border-transparent inline-flex items-center justify-center whitespace-nowrap py-2.5 px-7 text-base font-medium hover:ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-primary data-[state=active]:font-semibold `,
 				variant === 'secondary'
-					? 'data-[state=active]:border-b-2 data-[state=active]:border-primary'
-					: 'rounded-md data-[state=active]:bg-background data-[state=active]:text-primary data-[state=active]:font-semibold data-[state=active]:border data-[state=active]:shadow',
+					? 'data-[state=active]:border-primary'
+					: 'rounded-md data-[state=active]:bg-background data-[state=active]:text-primary data-[state=active]:shadow',
 				className
 			)}
 			{...props}
