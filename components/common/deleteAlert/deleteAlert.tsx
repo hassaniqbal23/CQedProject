4ac12c4e7a1ecbd0e863100b8248f69a735699cc
arslan <arslanshaheen1999@gmail.com>
2024-04-23 00:48:34 +0500

    open delete alert

--- components/common/deleteAlert/deleteAlert.tsx
+++ components/common/deleteAlert/deleteAlert.tsx
@@ -1,45 +1,70 @@
-import React, { useState } from "react";
+import React, { FC, useState } from "react";
 import Image from "next/image";
 // import Bin from "../public/icons/Bin.png";
 import { Button } from "@/components/ui";
+import Modal from "../Modal/Modal";
+import { Separator } from "@/components/ui/separator/separator";
 
-const Delete = () => {
-  const [isVisible, setIsVisible] = useState(true);
+interface IProps {
+  onClose: () => void;
+  onConfirm: () => void;
+  isVisible: boolean;
+}
 
-  const handleYesClick = () => {
-    setIsVisible(false);
-  };
+const Delete: FC<IProps> = ({ onClose, onConfirm, isVisible }) => {
+  //   const [isVisible, setIsVisible] = useState(false);
+
+  //   const handleYesClick = () => {
+  //     setIsVisible(false);
+  //   };
+
+  //   const handleNoClick = () => {
+  //     setIsVisible(false);
+  //   };
 
   return (
-    isVisible && (
-      <div className="w-full md:w-[458px] bg-white rounded-lg shadow-md  border-gray-300">
-        <div className="flex flex-col md:flex-row justify-start md:gap-12 border-b-2 border-gray-300">
-          <div className="md:max-w-[100px]">
-            <Image src="/icons/Bin.svg" alt="Bin" width={60} height={60} />
-          </div>
-          <div className="heading flex-grow">
-            <h2 className="text-foreground font-montserrat text-2xl font-semibold pb-2 md:mt-0">
-              Are you sure?
-            </h2>
-            <p className="text-gray-700 font-montserrat text-base font-medium mb-4">
-              Are you sure you want to remove invite
-            </p>
-          </div>
-        </div>
-        <div className="wrap-button flex flex-col md:flex-row justify-end md:items-center gap-4 md:gap-14 mt-4 md:mt-6">
-          <Button className="btn1 h-10 md:h-[42px] px-6 md:px-[40px] justify-center items-center flex-shrink-0 rounded-full shadow-btn font-bold font-montserrat text-base border border-primary-500 text-blueviolet bg-white">
+    <Modal
+      isVisible={isVisible}
+      // onClose={handleNoClick}
+      header={false}
+      footer={
+        <div className="wrap-button flex flex-col md:flex-row justify-end md:items-center gap-4 mt-4 md:mt-6">
+          <Button
+            className="btn1 h-10 md:h-[42px] px-6 md:px-[40px] justify-center items-center flex-shrink-0 rounded-full shadow-btn font-bold font-montserrat text-base border border-primary-500 text-[#4146B8] bg-white"
+            onClick={() => onClose()}
+          >
             No
           </Button>
 
           <Button
             className="btn2 h-10 md:h-[42px] px-6 md:px-[40px] justify-center items-center flex-shrink-0 rounded-full shadow-btn font-bold font-montserrat text-base bg-primary-500 text-white  "
-            onClick={handleYesClick}
+            onClick={() => onConfirm()}
           >
             Yes
           </Button>
         </div>
+      }
+    >
+      <div className="w-full md:w-[458px] bg-white rounded-lg   border-gray-300">
+        <div className="flex flex-col md:flex-row justify-start items-center gap-5 border-gray-300">
+          <div className="md:max-w-[100px] mb-4">
+            <Image src="/icons/Bin.svg" alt="Bin" width={55} height={60} />
+          </div>
+          <div className="heading">
+            <h2 className="text-foreground font-montserrat text-3xl font-semibold pb-2 md:pb-0">
+              Are you sure?
+            </h2>
+            <p className="text-gray-700 font-montserrat text-base font-medium mb-3">
+              Are you sure you want to remove invite
+            </p>
+          </div>
+        </div>
       </div>
-    )
+      <Separator
+        className=" w-full  space-x-10 text-sm bg-[#E6E6E6]
+"
+      />
+    </Modal>
   );
 };
 
--- components/common/deleteAlert/deteteAlert.stories.tsx
+++ components/common/deleteAlert/deteteAlert.stories.tsx
@@ -3,30 +3,48 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Delete from "./deleteAlert";
 import { Button } from "@/components/ui";
 import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 const meta: Meta<typeof Delete> = {
   title: "deleteAlert",
   component: Delete,
-};
+  parameters: {
+    layout: "centered",
+  },
+
+  tags: ["autodocs"],
+} satisfies Meta<typeof Delete>;
 
 export default meta;
 type Story = StoryObj<typeof Delete>;
 
 export const Default: Story = {
-  render() {
+  render(args) {
+    const [open, setOpen] = useState(false);
+    const value = {
+      ...args,
+      isVisible: open,
+      onClose: () => setOpen(false),
+      onConfirm: () => setOpen(false),
+    };
+    console.log(value, "checking");
     return (
-      <Modal
-        header={false}
-        footer={false}
-        openModalButton={<Button> delete</Button>}
-        isVisible={true}
-        onOkClick={() => {
-          alert("dfd");
-        }}
-      >
-        <Delete />
-      </Modal>
+      <>
+        <h3 onClick={() => setOpen(true)} className={"text-red-700"}>
+          {" "}
+          Open Delete Model{" "}
+        </h3>
+        <Delete {...value} />
+      </>
     );
   },
-  args: {},
+  args: {
+    isVisible: false,
+    onClose: () => {
+      console.log("close");
+    },
+    onConfirm() {
+      console.log("confirm");
+    },
+  },
 };
