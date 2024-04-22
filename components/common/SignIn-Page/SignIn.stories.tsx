63a4a4d097ef3c633146c165681c7e50fbc1561a
Sohail Abbas <sohalidatoo@gmail.com>
2024-04-22 18:03:56 +0500

    Login-Component

---
+++ components/common/SignIn-Page/SignIn.stories.tsx
@@ -0,0 +1,18 @@
+// stories/ProfileForm.stories.ts
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { SignIn } from '@/components/common/SignIn-Page/SignInpage';
+
+const meta = {
+  title: 'Ui/SignIn-Page',
+  component: SignIn,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof SignIn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
