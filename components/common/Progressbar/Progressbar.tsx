0fc524f35ce2eeb037ca65d381a241bafc0318f4
alihussain575 <alihussainirfan575@gmail.com>
2024-04-22 18:43:21 +0500

    created progress bar component

--- components/common/Progressbar/Progressbar.tsx
+++ components/common/Progressbar/Progressbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaCheckCircle } from 'react-icons/fa';
+import Image from 'next/image';
 
 interface IProgressbarprops {
     heading: string;
@@ -9,26 +10,39 @@ interface IProgressbarprops {
 function Progressbar({ percentage, heading }: IProgressbarprops) {
     return (
         <div className='w-full rounded-full'>
-            <div className='relative w-full bg-gray-200 rounded-full'>
-                <div
-                    className='absolute bg-secondary z-10 rounded-full transition-all duration-75 h-full'
-                    style={{
-                        width: `${percentage}%`,
-                        padding: '0.5rem'
-                    }}
-                >
-                    <span
-                        className='absolute right-0 transition-all duration-75 transform -translate-x-1/2'
+            <div className='w-full bg-primary-50 rounded-full' >
+                <div className='relative w-full rounded-full'>
+                    <div
+                        className='absolute bg-secondary z-20 rounded-full transition-all duration-75 h-full'
                         style={{
-                            top: '50%',
-                            transform: 'translateY(-50%)'
+                            width: `${percentage}%`,
+                            padding: '0.5rem'
                         }}
                     >
-                        <FaCheckCircle color='white' />
-                    </span>
+                        <span
+                            className='absolute -right-1 transition-all duration-75 transform -translate-x-1/2'
+                            style={{
+                                top: '50%',
+                                transform: 'translateY(-50%)'
+                            }}
+                        >
+                            <Image src='/Cartoon.svg' width={40} height={40} alt='cartoon image' />
+                        </span>
+                    </div>
+                    <div
+                        className='absolute bg-primary-50 z-10 rounded-full transition-all duration-75 h-full'
+                        style={{
+                            width: `100%`,
+                            padding: '0.5rem'
+                        }}
+                    >
+                    </div>
                 </div>
             </div>
-            <h2 className={`pt-3 pl-2 ${percentage == 100 ? 'text-succes' : ''}`} >{heading}</h2>
+            <div className='flex justify-between pt-3 pl-2 ' >
+                <h2 className={`font-semibold ` + (percentage === 100 ? 'text-success' : '')} >{heading}</h2>
+                <h2 >{percentage}% Completed</h2>
+            </div>
         </div>
     );
 }
