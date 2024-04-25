2225326e54c49288015c0017d273275c57694f68
Sohail Abbas <sohalidatoo@gmail.com>
2024-04-25 16:36:56 +0500

    Login Slider

---
+++ components/ui/carousel/carousel.stories.tsx
@@ -0,0 +1,46 @@
+import { LoginCarousel } from '@/components/ui/carousel/carousel';
+import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Card, CardContent } from '../card/card';
+
+const meta = {
+  title: 'Ui/LoginCarousel',
+  component: LoginCarousel,
+  parameters: {
+    layout: 'centered',
+  },
+
+  tags: ['autodocs'],
+} as Meta<typeof LoginCarousel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: (args) => {
+    return (
+      <>
+        <LoginCarousel carouselItems={args.carouselItems} />
+      </>
+    );
+  },
+  args: {
+    carouselItems: [
+      {
+        title: 'Join the Future',
+        description: 'Shaping Global Citizens T hrough Cultural Intelligence',
+        imgPath: '/assets/images/slider1.jpeg',
+      },
+      {
+        title: 'Join the Future',
+        description: 'Shaping Global Citizens T hrough Cultural Intelligence',
+        imgPath: '/assets/images/slider2.jpeg',
+      },
+      {
+        title: 'Join the Future',
+        description: 'Shaping Global Citizens T hrough Cultural Intelligence',
+        imgPath: '/assets/images/slider3.jpeg',
+      },
+    ],
+  },
+};
