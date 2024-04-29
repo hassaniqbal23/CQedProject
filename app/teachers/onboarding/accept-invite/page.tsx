4f27d38908b3e941f32ce8fc1bbb5cfaaa50b0d4
sajidnagari <sajidnagari5@gmail.com>
2024-04-29 19:38:40 +0300

    Rename import in accept-invite

--- app/teachers/onboarding/accept-invite/page.tsx
+++ app/teachers/onboarding/accept-invite/page.tsx
@@ -1,14 +1,12 @@
 'use client';
-import { CreateProfile as Profile } from '@/components/common/teacherProfile/create-profile';
-
-import BottomNavbar from '@/components/common/navbar/bottomNavbar';
+import { CreateProfile } from '@/components/common/teacherProfile/create-profile';
 import TopNavbar from '@/components/common/navbar/TopNavbar';
 
 export default function TeacherAcceptInvite() {
   return (
     <div>
       <TopNavbar onLogout={() => {}}></TopNavbar>
-      <Profile></Profile>
+      <CreateProfile></CreateProfile>
     </div>
   );
 }
