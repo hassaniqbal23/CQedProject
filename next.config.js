d86fcfecc415b99f6629379d0e1b0d1e7201557d
alihussain575 <alihussainirfan575@gmail.com>
2024-04-30 04:23:57 +0500

    added rewrites

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_HOST}/:path*`,
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
