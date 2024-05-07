d393f49333ad6d145a0b75a40beeb97071c88ee9
Ilyas Karim <ilyas.datoo@gmail.com>
2024-05-07 05:07:45 +0500

    Update communityCard.tsx

--- components/common/postCard/communitycard/communityCard.tsx
+++ components/common/postCard/communitycard/communityCard.tsx
@@ -7,9 +7,9 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card/card"
+} from '@/components/ui/card/card';
 import { Button, ButtonProps } from '@/components/ui';
-import { Users ,MessageCircle} from 'lucide-react';
+import { Users, MessageCircle } from 'lucide-react';
 
 interface IProps {
   totalMembers: number | string;
@@ -17,39 +17,44 @@ interface IProps {
   title: string;
   imageSrc: string;
   buttonProps: ButtonProps;
-
 }
 
 const Coummuntiycard: FC<IProps> = ({
-   totalMembers, 
-   totalDiscussions, 
-   title, 
-   imageSrc, 
-   buttonProps
-   }) => {
+  totalMembers,
+  totalDiscussions,
+  title,
+  imageSrc,
+  buttonProps,
+}) => {
   return (
-    <div >
+    <div>
       <Card className="flex w-full p-4 items-center gap-18 rounded-10 bg-primary-50 border border-gray-800">
         <Avatar className="w-20 h-20 md:w-54 md:h-54 rounded-full bg-lightgray mb-3 border border-gray-950 ">
           <AvatarImage src={imageSrc} alt="Profile Picture" />
         </Avatar>
         <CardHeader>
           <CardTitle>{title}</CardTitle>
-          <div className='flex'> 
-            <Users size={20} className='text-[#464650]'/> 
+          <div className="flex">
+            <Users size={20} className="text-[#464650]" />
             <CardDescription>{totalMembers} Members</CardDescription>
           </div>
-          <div className='flex'>
-            <MessageCircle size={20} className='text-[#464650]'/>
+          <div className="flex">
+            <MessageCircle size={20} className="text-[#464650]" />
             <CardDescription>{totalDiscussions}+ Discussions</CardDescription>
           </div>
         </CardHeader>
         <CardContent>
-          <Button {...buttonProps} className="flex h-[30px] max-w-[222px] px-[16px] justify-center items-center rounded-xl mt-8"> Join</Button>
+          <Button
+            {...buttonProps}
+            className="flex h-[30px] max-w-[222px] px-[16px] justify-center items-center rounded-xl mt-8"
+          >
+            {' '}
+            Join
+          </Button>
         </CardContent>
       </Card>
     </div>
   );
-}
+};
 
 export default Coummuntiycard;
