08bde216a0fb27b195b98f66ab951326ca478267
Sohail Abbas <sohalidatoo@gmail.com>
2024-05-08 01:55:17 +0500

    DeleteClass-Modal

--- app/api/admin.ts
+++ app/api/admin.ts
@@ -1,10 +1,15 @@
-import http from "../utils/http";
+import http from '../utils/http';
 
+export const getInvitedSchools: any = (
+  page: string | number = 1,
+  pageSize: number | string = 10
+) => {
+  return http.get(`/schools/all-schools?page=${page}&limit=${pageSize}`);
+};
 
-export const getInvitedSchools: any = (page: string | number = 1, pageSize: number | string = 10) => {
-    return http.get(`/schools/all-schools?page=${page}&limit=${pageSize}`)
-}
-
-export const getInvites = (page: string | number = 1, pageSize: number | string = 10) => {
-    return http.get(`/invitation/all-invites?page=${page}&limit=${pageSize}`)
-}
+export const getInvites = (
+  page: string | number = 1,
+  pageSize: number | string = 10
+) => {
+  return http.get(`/invitation/all-invites?page=${page}&limit=${pageSize}`);
+};
