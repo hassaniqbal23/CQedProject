986add707311a3dbe309886dcdfaaf4925680f89
alihussain575 <alihussainirfan575@gmail.com>
2024-05-17 16:34:55 +0500

    created autorezise input

---
+++ components/Chat/ChatContent/ChatInput/ChatInput.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ChatInput } from './ChatInput';
+
+const meta: Meta<typeof ChatInput> = {
+    title: 'Chat/ChatInput',
+    component: ChatInput,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ChatInput>;
+
+export const chatDafualt: Story = {};
