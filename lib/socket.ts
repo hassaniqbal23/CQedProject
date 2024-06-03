0ca45265267c43c6c43a02eae0503e3f89506f9d
Ilyas Karim <ilyas.datoo@gmail.com>
2024-06-03 20:03:21 +0500

    Update socket.ts

--- lib/socket.ts
+++ lib/socket.ts
@@ -3,23 +3,24 @@ import { io } from 'socket.io-client';
 
 let URL = process.env.NEXT_PUBLIC_API_HOST || '';
 
-const options = {
+if (process.env.NODE_ENV === 'production') {
+  URL = 'https://cqed-dev.staginguconnect.com';
+}
+
+console.log('Connecting to socket server', URL);
+export const socket = io(URL, {
   autoConnect: false,
   auth: {
     token: typeof window !== 'undefined' ? getAccessToken() : '',
   },
-  transport: ['websocket'],
+  transports: ['websocket'],
   upgrade: false,
   forceNew: true,
   reconnectionDelay: 3000,
   reconnection: true,
   reconnectionAttempts: Infinity,
-  jsonp: false,
-  path: process.env.NODE_ENV === 'production' ? '/api/socket.io' : '/socket.io',
-};
-
-console.log('Connecting to socket server', URL);
-export const socket = io(URL, options);
+  path: '/api/socket.io',
+});
 
 export function connect() {
   const token = getAccessToken();
