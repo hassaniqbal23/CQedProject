c5ec325b5ea5cdda38a0ae2e92fdfd85b1bb6821
Ilyas Karim <ilyas.datoo@gmail.com>
2024-06-18 09:29:17 +0500

    Update FileUploader.tsx

--- components/ui/FileUploader/FileUploader.tsx
+++ components/ui/FileUploader/FileUploader.tsx
@@ -162,7 +162,6 @@ export const FileUploader = forwardRef<
         const newValues: File[] = value ? [...value] : [];
 
         if (reSelectAll) {
-          console.log('slice');
           newValues.splice(0, newValues.length);
         }
 
