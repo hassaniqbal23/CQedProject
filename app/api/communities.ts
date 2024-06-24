517ba1a490ce4ac9afc339851dcb71ae90c1ac9a
alihussain575 <alihussainirfan575@gmail.com>
2024-06-25 01:23:30 +0500

    fixed design and some issues in dashboard feeds

--- app/api/communities.ts
+++ app/api/communities.ts
@@ -187,7 +187,6 @@ export const pendingCommunities = () => {
     .then((res) => res.data);
 };
 
-
-export const getSuggestedCommunities = ()=> {
+export const getSuggestedCommunities = () => {
   return http.get(`/community/suggest-communities`).then((res) => res.data);
-}
+};
