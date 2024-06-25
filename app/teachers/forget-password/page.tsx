61d7b53bc7df3601f1341a6d64ae584f7d221528
AyazAslam101 <ayazdattuu101@gmail.com>
2024-06-25 15:29:24 +0500

    changes

--- app/teachers/forget-password/page.tsx
+++ app/teachers/forget-password/page.tsx
@@ -9,17 +9,14 @@ export default function TeacherForgetPassword() {
   return (
     <>
       <TopNavbar showLogout={false}></TopNavbar>
-      <div className="grid grid-cols-1 md:grid-cols-1 items-center">
-        <div className="flex justify-center items-center bg-white rounded-md shadow-md pt-[24px] px-[40px] pb-[45px] w-4/6 md:w-2/5 lg:w-2/6 mx-auto">
-          <div className="bg-secondary-light w-full md:w-9/9 flex flex-col gap-[30px]  ">
-            <ForgetPassword
-              onSubmit={() => {
-                router.push('/teachers/forget-password/email-sent');
-              }}
-              onClick={() => {}}
-            ></ForgetPassword>
-          </div>
-        </div>
+      <div className="max-w-md mx-auto flex justify-center items-center h-[80vh] p-2 ">
+
+        <ForgetPassword
+          onSubmit={() => {
+            router.push('/teachers/forget-password/email-sent');
+          }}
+          onClick={() => { }}
+        ></ForgetPassword>
       </div>
     </>
   );
