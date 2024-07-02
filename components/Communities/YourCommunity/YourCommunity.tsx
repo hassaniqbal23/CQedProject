ccffb1ed034089be154c8f379d4a2c041393f80c
Ilyas Karim <ilyas.datoo@gmail.com>
2024-07-03 03:45:13 +0500

    Added another api for communities requested

--- components/Communities/YourCommunity/YourCommunity.tsx
+++ components/Communities/YourCommunity/YourCommunity.tsx
@@ -12,7 +12,11 @@ import { Plus } from 'lucide-react';
 import { CommunityCard } from '../CommunityCard2/CommunityCard2';
 import { useRouter } from 'next/navigation';
 import { useQuery, useQueryClient } from 'react-query';
-import { getCommunityJoined, getMyCommunity } from '@/app/api/communities';
+import {
+  getCommunityJoined,
+  getCommunityRequested,
+  getMyCommunity,
+} from '@/app/api/communities';
 import { ICommunity, ICommunityJoined } from '@/types/community';
 import { useGlobalState } from '@/app/globalContext/globalContext';
 
@@ -27,6 +31,13 @@ export const YourCommunity: FC<IProps> = ({ module = 'students' }) => {
   // useEffect(() => {
   //   client.refetchQueries('UserJoinedCommunities')
   // }, [joinedCommunities])
+
+  const {
+    data: communitiesRequested,
+    isLoading: isFetchingRequestedCommunities,
+  } = useQuery(['getCommunityRequested', 1, 10], () =>
+    getCommunityRequested(1, 10)
+  );
 
   const { data: communityJoined, isLoading: isFetchingCommunityJoined } =
     useQuery(['getCommunityJoined', 1, 10], () => getCommunityJoined(1, 10), {
@@ -89,6 +100,14 @@ export const YourCommunity: FC<IProps> = ({ module = 'students' }) => {
                 />
               );
             })}
+          {mycommunity?.data?.length === 0 &&
+            isFetchingCommunityJoined === false && (
+              <div className="flex justify-center items-center h-40">
+                <Typography variant="h5" weight="semibold">
+                  You have not created any communities yet
+                </Typography>
+              </div>
+            )}
           {mycommunity &&
             mycommunity?.data?.map((item: ICommunity, index: number) => {
               return (
@@ -154,8 +173,79 @@ export const YourCommunity: FC<IProps> = ({ module = 'students' }) => {
                 />
               );
             })}
+          {communityJoined?.data?.length === 0 &&
+            isFetchingMycommunity === false && (
+              <div className="flex justify-center items-center h-40">
+                <Typography variant="h5" weight="semibold">
+                  You have not joined any communities yet
+                </Typography>
+              </div>
+            )}
           {communityJoined &&
             communityJoined?.data?.map(
+              (item: ICommunityJoined, index: number) => {
+                return (
+                  <CommunityCard
+                    loading={false}
+                    description={item.description}
+                    title={item.name}
+                    id={item.id}
+                    members={item._count.CommunityUsers}
+                    key={index}
+                    image={item?.profile_picture.file_path}
+                    module={module ? module : 'students'}
+                  />
+                );
+              }
+            )}
+        </CardContent>
+      </Card>
+      <Card className="rounded-xl px-5 mt-10">
+        <CardHeader className="p-5">
+          <div className="flex items-center justify-between flex-wrap">
+            <Typography
+              variant="h4"
+              weight="semibold"
+              className="flex gap-3 items-end"
+            >
+              <Image
+                src={'/yellowChart.svg'}
+                alt="chart"
+                className="w-10 h-10"
+                width={25}
+                height={25}
+              />
+              Communities you have Requested
+            </Typography>
+          </div>
+        </CardHeader>
+        <Separator className="m-0" />
+        <CardContent>
+          {isFetchingRequestedCommunities &&
+            [1, 2, 3].map((i) => {
+              return (
+                <CommunityCard
+                  loading={isFetchingRequestedCommunities}
+                  description={''}
+                  title={''}
+                  id={i}
+                  members={0}
+                  key={i}
+                  image={''}
+                  module={module ? module : 'students'}
+                />
+              );
+            })}
+          {communitiesRequested?.data?.length === 0 &&
+            isFetchingRequestedCommunities === false && (
+              <div className="flex justify-center items-center h-40">
+                <Typography variant="h5" weight="semibold">
+                  You have not requested to join any communities yet
+                </Typography>
+              </div>
+            )}
+          {communitiesRequested &&
+            communitiesRequested?.data?.map(
               (item: ICommunityJoined, index: number) => {
                 return (
                   <CommunityCard
